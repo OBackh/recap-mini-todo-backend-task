2ddaa1955902e266f6e182ec60006de6df94063d
Ole Backhaus <ole.backhaus@gmx.de>
2024-10-29 12:13:30 +0100

    integrated GlobalExceptionHandler

---
+++ src/main/java/de/neuefische/todobackend/todo/exceptions/CustomErrorObject.java
@@ -0,0 +1,4 @@
+package de.neuefische.todobackend.todo.exceptions;
+
+public record CustomErrorObject( String message ) {
+}
---
+++ src/main/java/de/neuefische/todobackend/todo/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,16 @@
+package de.neuefische.todobackend.todo.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public CustomErrorObject handleRuntimeException(RuntimeException exception) {
+        return new CustomErrorObject(exception.getMessage());
+    }
+}
